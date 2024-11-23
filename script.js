638cd60c09b8f081a553d281f9ae889d8e39a427
vitaliymv <movcan77@gmail.com>
2024-11-23 11:51:31 +0200

    add post, get request

--- script.js
+++ script.js
@@ -12,5 +12,33 @@ document.querySelector("#new").addEventListener("click", () => {
 })
 
 fetch(url + ownerKey).then(async function (response) {
-    console.log(await response.json());
+    render(await response.json())
 })
+
+function render(cards) {
+    let tbody = document.querySelector("tbody");
+    tbody.innerHTML = null;
+    
+    for (const card of cards) {
+        let formattedNumber = card.cardNumber.match(/.{1,4}/g).join(" ");
+        tbody.innerHTML += `
+            <tr>
+                <td>${card.id}</td>
+                <td>${formattedNumber}</td>
+                <td>${card.expireDate}</td>
+                <td>${card.balance}₴</td>
+                <td><input type="password" value="${card.cvv}" readonly></td>
+                <td>
+                    <button class="btn btn-warning">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                </td>
+                <td>
+                    <button class="btn btn-danger">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </td>
+            <tr>
+        `
+    }
+}
